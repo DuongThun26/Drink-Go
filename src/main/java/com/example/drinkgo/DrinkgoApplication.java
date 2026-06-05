e88b21447c7be4ac7200a4bb5953bba9fb3a5c23
DuongThun26 <duongvanthuan2k4@gmail.com>
2026-06-05 16:48:16 +0700

    Khởi tạo dự án

---
+++ src/main/java/com/example/drinkgo/DrinkgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.drinkgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkgoApplication.class, args);
+	}
+
+}
